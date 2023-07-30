b3d92036d44d9081f9a46f1215649fc7c9a28eb4
irfansud2nd <irfansud2nd@gmail.com>
2023-07-30 20:15:05 +0700

    about's section centered

--- components/About.tsx
+++ components/About.tsx
@@ -10,17 +10,17 @@ const About = () => {
       className="flex justify-center items-center"
       data-section
     >
-      <div className="relative min-h-[500px] max-w-2xl flex flex-col justify-center items-center">
+      <div className="relative min-h-[500px] max-w-[1440px] min-[1440px]:mx-auto px-5 lg:px-40 flex flex-col justify-center items-center">
         <Image
           alt="about banner"
           src="/images/about-banner.jpg"
           width={300}
           height={500}
-          className="object-cover absolute -z-[1] top-1/2 -translate-y-1/2 -left-[160px]"
+          className="object-cover self-start hidden md:block absolute -z-[1]"
         />
-        <div className=" bg-gray-200 px-5 py-2">
+        <div className=" bg-gray-200 px-5 py-2 md:ml-[25%]">
           <h1 className="font-semibold text-3xl">About Me</h1>
-          <h2 className="font-semobold text-xl text-justify">
+          <h2 className="font-semobold text-xl">
             I am a graduated automation engineer who wants to be a frontend
             developer
           </h2>
