a20da4a9c5c65f688f42a16741a57253641291f0
irfansud2nd <irfansud2nd@gmail.com>
2024-01-21 23:23:57 +0700

    remove next_public prefix

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -7,12 +7,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_USER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_USER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
